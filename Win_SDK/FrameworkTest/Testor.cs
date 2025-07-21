e3ec919f16d2acdfc8b82fd7861cbd3d8c70a922
luckyyuqiang <luckyyuqiang@163.com>
2025-07-21 17:32:21 +0800

    change code for init option.

--- Win_SDK/FrameworkTest/Testor.cs
+++ Win_SDK/FrameworkTest/Testor.cs
@@ -2493,11 +2493,13 @@ namespace WinSDKTest
             //Options options = new Options("5101220107132865#test"); // 北京沙箱测试环境，无法正常登录
             //Options options = new Options("41117440#383391"); // 线上环境, demo中的token
 
-            //if (appkey.Length > 0 && appkey.Contains("#") == true)
-            //    options.AppKey = appkey;
+            Options options = Options.InitOptionsWithAppKey("easemob-demo#unitytest");
+
+            if (appkey.Length > 0 && appkey.Contains("#") == true)
+                options = Options.InitOptionsWithAppKey(appkey);
 
             //Options options = Options.InitOptionsWithAppKey("easemob-demo#sdk111");
-            Options options = Options.InitOptionsWithAppKey("easemob-demo#unitytest");
+            //Options options = Options.InitOptionsWithAppKey("easemob-demo#unitytest");
 
             options.AutoLogin = false;
             options.UsingHttpsOnly = true;
